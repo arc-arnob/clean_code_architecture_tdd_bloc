fc610428f0bc041b98afffe648d0fe38fda69943
Arnob  Chowdhury <arnobchaudhury@gmail.com>
2023-04-23 15:08:27 +0530

    part 3 added overview of data layer and models

--- test/features/number_trivia/data/models/number_trivia_model_test.dart
+++ test/features/number_trivia/data/models/number_trivia_model_test.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:clean_code_architecture_tdd/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:clean_code_architecture_tdd/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+import '../../../../core/fixtures/fixture_reader.dart';
 
 void main() {
   final tNumberTriviaModel = NumberTriviaModel(number: 1, text: 'test text');
@@ -11,4 +15,39 @@ void main() {
       expect(tNumberTriviaModel, isA<NumberTrivia>());
     },
   );
+  group('fromJson', () {
+    test('Should return a valid model when the JSON number is an integer',
+        () async {
+      // arrange
+      // We want to get the fixture to simulate the JSON and convert it to Map
+      final Map<String, dynamic> jsonMap = json.decode(fixture('trivia.json'));
+      // act
+      final result = NumberTriviaModel.fromJson(jsonMap);
+      // assert
+      expect(result, tNumberTriviaModel);
+    });
+
+    test('Should return a valid model when the JSON number is an double',
+        () async {
+      // arrange
+      // We want to get the fixture to simulate the JSON and convert it to Map
+      final Map<String, dynamic> jsonMap =
+          json.decode(fixture('trivia_double.json'));
+      // act
+      final result = NumberTriviaModel.fromJson(jsonMap);
+      // assert
+      expect(result, tNumberTriviaModel);
+    });
+  });
+
+  group('toJson', () {
+    test('Should return a valid model when the JSON number is an integer',
+        () async {
+      // act
+      final result = tNumberTriviaModel.toJson();
+      // assert
+      final exectedMap = {"text": "Test text", "number": 1};
+      expect(result, exectedMap);
+    });
+  });
 }
