1745edb0b90a6deba9a6e8faeb27f46cb9652cf0
Arnob  Chowdhury <arnobchaudhury@gmail.com>
2023-04-25 09:26:36 +0530

    Added API call with TDD approach

--- lib/features/number_trivia/data/datasources/number_trivia_remote_datasource.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_remote_datasource.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:clean_code_architecture_tdd/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:http/http.dart' as http;
+
+import '../../../../core/error/exceptions.dart';
 
 abstract class NumberTriviaRemoteDataSource {
   /// Calls the http://numbersapi.com/{number} endpoint.
@@ -10,4 +15,32 @@ abstract class NumberTriviaRemoteDataSource {
   ///
   /// Throws a [ServerException] for all error codes.
   Future<NumberTriviaModel> getRandomNumberTrivia();
+}
+
+class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource {
+  final http.Client client;
+  NumberTriviaRemoteDataSourceImpl({required this.client});
+
+  @override
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int number) =>
+      _getTriviaFromUrl('http://numbersapi.com/$number');
+
+  @override
+  Future<NumberTriviaModel> getRandomNumberTrivia() =>
+      _getTriviaFromUrl('http://numbersapi.com/random');
+
+  Future<NumberTriviaModel> _getTriviaFromUrl(String url) async {
+    final response = await client.get(
+      Uri.parse(url),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return NumberTriviaModel.fromJson(json.decode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
 }
---
+++ test/features/number_trivia/data/datasource/number_trivia_remote_datasource_test.dart
@@ -0,0 +1,118 @@
+import 'dart:convert';
+
+import 'package:clean_code_architecture_tdd/core/error/exceptions.dart';
+import 'package:clean_code_architecture_tdd/features/number_trivia/data/datasources/number_trivia_local_datsource.dart';
+import 'package:clean_code_architecture_tdd/features/number_trivia/data/datasources/number_trivia_remote_datasource.dart';
+import 'package:clean_code_architecture_tdd/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+
+import 'number_trivia_remote_datasource_test.mocks.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
+
+@GenerateNiceMocks([MockSpec<http.Client>()])
+void main() {
+  late NumberTriviaRemoteDataSourceImpl dataSource;
+  late MockClient mockHttpClient;
+
+  setUp(() {
+    mockHttpClient = MockClient();
+    dataSource = NumberTriviaRemoteDataSourceImpl(client: mockHttpClient);
+  });
+  group('getConcreteNumberTrivia', () {
+    final tNumber = 1;
+    final tNumberTriviaModel =
+        NumberTriviaModel.fromJson(json.decode(fixture('trivia.json')));
+    test(
+        'should perform get request on url with number being the endpoint and application/json as header',
+        () async {
+      // arrange
+      when(mockHttpClient.get(any, headers: anyNamed('headers')))
+          .thenAnswer((_) async => http.Response(fixture('trivia.json'), 200));
+      // act
+      dataSource.getConcreteNumberTrivia(tNumber);
+      // verify
+      verify(mockHttpClient.get(Uri.parse('http://numbersapi.com/$tNumber'),
+          headers: {'Content-Type': 'application/json'}));
+    });
+
+    test('should return NumberTrivia when the response code is 200 (success)',
+        () async {
+      // arrange
+      when(mockHttpClient.get(any, headers: anyNamed('headers')))
+          .thenAnswer((_) async => http.Response(fixture('trivia.json'), 200));
+      // act
+      final result = await dataSource.getConcreteNumberTrivia(tNumber);
+      // verify
+      expect(result, equals(tNumberTriviaModel));
+    });
+    test(
+      'should throw a ServerException when the response code is 404 or other',
+      () async {
+        // arrange
+        when(mockHttpClient.get(any, headers: anyNamed('headers'))).thenAnswer(
+            (_) async => http.Response('Something went wrong', 404));
+        // act
+        final call = dataSource.getConcreteNumberTrivia;
+        // assert
+        expect(() => call(tNumber), throwsA(TypeMatcher<ServerException>()));
+      },
+    );
+  });
+
+  group('getRandomNumberTrivia', () {
+    final tNumberTriviaModel =
+        NumberTriviaModel.fromJson(json.decode(fixture('trivia.json')));
+
+    test(
+      '''should perform a GET request on a URL with number
+       being the endpoint and with application/json header''',
+      () async {
+        // arrange
+        when(mockHttpClient.get(any, headers: anyNamed('headers'))).thenAnswer(
+            (_) async => http.Response(fixture('trivia.json'), 200));
+        // act
+        dataSource.getRandomNumberTrivia();
+        // assert
+        verify(mockHttpClient.get(
+          Uri.parse('http://numbersapi.com/random'),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        ));
+      },
+    );
+
+    test(
+      'should return NumberTrivia when the response code is 200 (success)',
+      () async {
+        // arrange
+        when(mockHttpClient.get(any, headers: anyNamed('headers'))).thenAnswer(
+            (_) async => http.Response(fixture('trivia.json'), 200));
+        // act
+        final result = await dataSource.getRandomNumberTrivia();
+        // assert
+        expect(result, equals(tNumberTriviaModel));
+      },
+    );
+
+    test(
+      'should throw a ServerException when the response code is 404 or other',
+      () async {
+        // arrange
+        when(mockHttpClient.get(any, headers: anyNamed('headers'))).thenAnswer(
+            (_) async => http.Response('Something went wrong', 404));
+        // act
+        final call = dataSource.getRandomNumberTrivia;
+        // assert
+        expect(() => call(), throwsA(TypeMatcher<ServerException>()));
+      },
+    );
+  });
+}
+
+
+// flutter pub run build_runner build                    
