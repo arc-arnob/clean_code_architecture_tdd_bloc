f68b16632759e143a1ea520bc7e21f56ebfbc39d
Arnob  Chowdhury <arnobchaudhury@gmail.com>
2023-04-27 00:57:55 +0530

    Started UI layer

---
+++ lib/features/number_trivia/presentation/pages/number_trivia_page.dart
@@ -0,0 +1,75 @@
+import 'package:clean_code_architecture_tdd/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
+import 'package:clean_code_architecture_tdd/injection_container.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class NumberTriviaPage extends StatelessWidget {
+  const NumberTriviaPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Clean Code Architecture"),
+      ),
+      body: buildBody(context),
+    );
+  }
+
+  BlocProvider<NumberTriviaBloc> buildBody(BuildContext context) {
+    return BlocProvider(
+      create: (BuildContext context) => sl<NumberTriviaBloc>(),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 10,
+              ),
+              // Top half
+              // BlocBuilder<NumberTriviaBloc, NumberTriviaState>(
+              //   builder: (context, state) {
+              //     return Container(
+              //       height: MediaQuery.of(context).size.height / 3,
+              //       child: Placeholder(),
+              //     );
+              //   },
+              // ),
+              SizedBox(
+                height: 20,
+              ),
+              // Bottom Half
+              Column(
+                children: <Widget>[
+                  Placeholder(
+                    fallbackHeight: 40,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Placeholder(fallbackHeight: 30),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Expanded(
+                        child: Placeholder(fallbackHeight: 30),
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
